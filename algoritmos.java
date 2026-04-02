c633d0e75f67a5c94853f23b0b098e6acdbceda1
Matheus Pereira <tuliocorreamatheus@gmail.com>
2026-04-02 19:25:06 -0300

    feat: adiciona exercício 01 - exibição de mensagem 20 vezes

---
+++ algoritmos.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Programar {
+    public static void main(String[] args) { 
+        Scanner sc = new Scanner(System.in);
+
+        for (int i = 1; i <= 20; i++) {
+            System.out.println("Eu gosto de estudar Algoritmos!");
+        }
+
+        sc.close();
+    }
+}
